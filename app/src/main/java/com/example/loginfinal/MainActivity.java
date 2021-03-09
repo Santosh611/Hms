96b5f22c58dd79e6365b0d93a7e4f17eb68d283d
Santosh A M <santoshalamandha2000@gmail.com>
2021-03-09 14:24:23 +0530

    Final commit

--- app/src/main/java/com/example/loginfinal/MainActivity.java
+++ app/src/main/java/com/example/loginfinal/MainActivity.java
@@ -1,46 +1,115 @@
 package com.example.loginfinal;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.Objects;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.regex.Pattern;
+
 public class MainActivity extends AppCompatActivity {
-    EditText username,password;
-    Button login;
+
+    private EditText Name;
+    private EditText Password;
+    private Button Login;
+    boolean isValid;
+    private static final Pattern PASSWORD_PATTERN = Pattern.compile("^" +
+            "(?=.*[0-9])" +
+            "(?=.*[a-z])" +
+            "(?=.*[A-Z])" +
+            ".{6,}" + "$");
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        username=findViewById(R.id.username);
-        password=findViewById(R.id.password);
-        login=findViewById(R.id.login);
-        login.setOnClickListener(new View.OnClickListener() {
+
+
+        Name = (EditText) findViewById(R.id.etUserName);
+        Password = (EditText) findViewById(R.id.etPassword);
+        Login = (Button) findViewById(R.id.btnLogin);
+
+
+        Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String mEmail = username.getText().toString().trim();
-                String mPass = password.getText().toString().trim();
+                String iname=Name.getText().toString();
+                String ipwd=Password.getText().toString();
 
-                if(!mEmail.isEmpty() || !mPass.isEmpty()){
-//                    Login1(username, password);
-
-                } else {
-                    username.setError("Please insert email");
-                    password.setError("Please insert password");
+                if(ipwd.isEmpty())
+                {
+                    Password.setError("Feild can't be empty");
+                }
+                else if(!PASSWORD_PATTERN.matcher(ipwd).matches()){
+                    Password.setError("Password is WEAK");
+                }
+                else {
+                    isValid = get_json(iname,ipwd);
+                    if(!isValid) {
+                        Toast.makeText(MainActivity.this, "Incorrect Credentials", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                        startActivity(intent);
+                    }
+                }
                 }
 
-
-
-                }
+//            }
         });
+
+    }
+
+
+    public boolean get_json(String uname, String upwd) {
+        String json;
+        int check=0;
+        try {
+
+            InputStream is = getAssets().open("data.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+            JSONArray jsonArray = new JSONArray(json);
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject obj = jsonArray.getJSONObject(i);
+                if (obj.getString("username").equals(uname) ){
+                    if (obj.getString("password").equals(upwd)) {
+                        check=1;
+                    }
+                }
+            }
+
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+        if(check==1) {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+
     }
 }
-
